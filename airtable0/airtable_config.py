c655715598f688fe59228b43741dd2393e3f5378
Adhdhyan Jain <adhdhyan.jain@gmail.com>
2025-07-30 22:03:30 +0530

    update

--- airtable0/airtable_config.py
+++ airtable0/airtable_config.py
@@ -1,7 +1,6 @@
 import os
 from dotenv import load_dotenv
-load_dotenv()
-import os
+load_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 
 AIRTABLE_API_KEY = os.getenv("AIRTABLE_API_KEY")
 BASE_ID = "applrq5qLHjJohD40"
